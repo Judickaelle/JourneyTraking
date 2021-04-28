214593bb95d85be8a4075cfae78c5039899af8eb
Judickaelle <judickaelle.pelletier@gmail.com>
2021-04-28 16:43:50 +0200

    add marker on map and draw polyline

--- app/src/main/java/com/judickaelle/pelletier/journeytracking/mainactivity/MapsActivity.java
+++ app/src/main/java/com/judickaelle/pelletier/journeytracking/mainactivity/MapsActivity.java
@@ -3,7 +3,6 @@ package com.judickaelle.pelletier.journeytracking.mainactivity;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentActivity;
 
 import android.annotation.SuppressLint;
 import android.graphics.Color;
@@ -16,8 +15,11 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
@@ -25,16 +27,29 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.judickaelle.pelletier.journeytracking.R;
 import com.judickaelle.pelletier.journeytracking.journey.JourneyItem;
+import com.judickaelle.pelletier.journeytracking.step.Step;
+
+import java.util.ArrayList;
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private final FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private DocumentSnapshot document;
     private CollectionReference stepbookRef;
     private String accesKey;
+    private Double latOne, longOne, latitude, longitude;
+    static boolean next = false;
+
+    private static ArrayList<Step> listStep = new ArrayList<Step>();
 
     private JourneyItem journeyItem;
+    private Step myStep;
+    private Step stepOne;
+    private Query query;
 
     private GoogleMap mMap;
 
@@ -65,41 +80,85 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         myJourney.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
             @Override
             public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                if(task.isSuccessful()){
-                    DocumentSnapshot document = task.getResult();
-                    if(document.exists()){
+                if (task.isSuccessful()) {
+                    document = task.getResult();
+                    if (document.exists()) {
                         Log.d("tag", "Journey exist : " + document.getData());
                         journeyItem = document.toObject(JourneyItem.class);
-                        Log.d("tag", "Journey title : "+ journeyItem.getTitle());
+                        Log.d("tag", "Journey title : " + journeyItem.getTitle());
 
                         //set title and subtitle on the action bar
                         actionBar.setTitle(getString(R.string.map_title) + " " + journeyItem.getTitle());
                         actionBar.setSubtitle(accesKey);
-                    }else {
+                    } else {
                         Log.d("tag", "no such document");
                     }
-                }else {
-                    Log.d("tag", "get document failed with : "+ task.getException());
+                } else {
+                    Log.d("tag", "get document failed with : " + task.getException());
                 }
             }
         });
+
+        //get step of the journey thanks to the idJourney
+        query = stepbookRef
+                .whereEqualTo("id_journey", accesKey)
+                .orderBy("stepNumber", Query.Direction.ASCENDING);
+        query.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    //Log.d("tag", "taille liste récupérée  : " + task.getResult().size());
+                    for (QueryDocumentSnapshot documentSnapshot : task.getResult()) {
+                        //Step step = documentSnapshot.toObject(Step.class);
+                        //Log.d("tag", "numéro step  : " + step.getStepNumber() + "  titre : " + step.getStepTitle());
+                        listStep.add(documentSnapshot.toObject(Step.class));
+                    }
+                } else {
+                    Log.e("tag", "Error getting documents: ", task.getException());
+                }
+            }
+        });
+
     }
 
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        //TODO get step of the journey thanks to the idJourney
-        Query query = stepbookRef
-                .whereEqualTo("id_journey", accesKey)
-                .orderBy("stepNumber" , Query.Direction.ASCENDING);
-
-        //TODO for each step display marker on the map
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //define a polyline
+        PolylineOptions polylineOptions = new PolylineOptions();
+
+        //initialization of the map and focus on the first marker
+        stepOne = listStep.get(0);
+        latOne = Double.parseDouble(stepOne.getLatitude());
+        longOne = Double.parseDouble(stepOne.getLongitude());
+        createMarker(mMap, latOne, longOne, stepOne.getStepTitle());
+        //move the camera
+        LatLng initial = new LatLng(latOne, longOne);
+        polylineOptions.add(initial);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(initial));
+        //zoom en 10x
+        CameraPosition cameraPos = new CameraPosition.Builder().target(initial).zoom(10).build();
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPos));
+
+
+        //add the other step on the map
+        for (int i = 1; i < listStep.size(); i++) {
+            myStep = listStep.get(i);
+            Log.d("tag", "step " + myStep.getStepTitle());
+            latitude = Double.parseDouble(myStep.getLatitude());
+            longitude = Double.parseDouble(myStep.getLongitude());
+            polylineOptions.add(new LatLng(latitude, longitude));
+            createMarker(mMap, latitude, longitude, myStep.getStepTitle());
+        }
+        mMap.addPolyline(polylineOptions);
+    }
+
+
+    protected void createMarker(GoogleMap googleMap, double lat, double lng, String title) {
+        Log.d("tag", "step " + title + " added to the map");
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(lat, lng)).title(title));
     }
 
     //finish the activity on button back press
